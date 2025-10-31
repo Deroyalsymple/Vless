3a6ff2891b08c63d16d10d692d59cf82284a7e0b
Deroyalsymple <deroyalsymple@gmail.com>
2025-10-31 08:02:39 -0400

    Delete Worker.js

--- Worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request) {
-    const url = new URL(request.url);
-    url.hostname = "104.21.12.34";  // ← US IP (Cloudflare NY)
-    url.port = "443";
-    const modifiedRequest = new Request(url.toString(), request);
-    return fetch(modifiedRequest);
-  }
-};
